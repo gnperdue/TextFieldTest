86e971f5365b236749703734ca42a1b76b82910f
Gabriel Perdue <gnperdue@hey.com>
2021-01-14 07:19:13 -0600

    implement solution proposed by Scott Grosch in RayWenderlich Discord - works!

--- TextFieldTest/ContentView.swift
+++ TextFieldTest/ContentView.swift
@@ -46,26 +46,46 @@ struct ContentView: View {
   }
 }
 
-
+/*
+ Attempt solution proposed by Scott Grosch in RayWenderlich Discord
+ */
 struct SheetView: View {
   @Environment(\.presentationMode) var mode: Binding<PresentationMode>
   @Binding var point: Point
+  @State private var xValue: String
+  @State private var yValue: String
+
+  init(point: Binding<Point>) {
+    _point = point
+    _xValue = State(initialValue: "\(point.wrappedValue.x)")
+    _yValue = State(initialValue: "\(point.wrappedValue.y)")
+    // actually prefer, but here, whatevs - neat to see how to pass above
+//    _xValue = State(initialValue: "")
+//    _yValue = State(initialValue: "")
+  }
   
   var body: some View {
     
     VStack {
-      Text("Plain old Point struct")
 
       Form {
         Section(header: Text("X").bold()) {
-          TextField("Set X", value: $point.x, formatter: NumberFormatter())
+          TextField("Set X", text: $xValue)
+            .keyboardType(.decimalPad) // .numberPad? - want a decimal though
         }
         Section(header: Text("Y").bold()) {
-          TextField("Set Y", value: $point.y, formatter: NumberFormatter())
+          TextField("Set Y", text: $yValue)
+            .keyboardType(.decimalPad) // .numberPad? - for ints, I guess
         }
       }
 
       Button(action: {
+        guard let x = Double(xValue), let y = Double(yValue) else {
+          // post a pop-up, etc.
+          return
+        }
+        point.x = x
+        point.y = y
         self.mode.wrappedValue.dismiss()
       }, label: {
         Label("Enter data", systemImage: "hand.point.up.left")
