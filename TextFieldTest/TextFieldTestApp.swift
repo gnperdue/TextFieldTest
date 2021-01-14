db9a23eaf0baeda378c2cb368cf9a3723d7c42e9
Gabriel Perdue <gnperdue@hey.com>
2021-01-13 18:11:05 -0600

    Initial Commit

---
+++ TextFieldTest/TextFieldTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldTestApp.swift
+//  TextFieldTest
+//
+//  Created by Gabriel Perdue on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
