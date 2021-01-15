f24519c0047df544e7b1acc59b88353a0fd824d3
Gabriel Perdue <gnperdue@hey.com>
2021-01-14 18:16:57 -0600

    add integer text field only value, and double value

--- TextFieldTest/IntegerTextFieldValue.swift
+++ TextFieldTest/IntegerTextFieldValue.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+public class IntegerTextFieldValue: ObservableObject {
+  @Published var value = "" {
+    didSet {
+      let numbersOnly = value.filter { $0.isNumber }
+      if value != numbersOnly {
+        value = numbersOnly
+      }
+    }
+  }
+}
+
+public class DoubleTextFieldValue: ObservableObject {
+
+  /// trim an input string into something we may cast to Double
+  func trimStr(str: String) -> String {
+    // if no ".", then just check for numbers
+    guard let first = str.firstIndex(of: ".") else {
+      return str.filter { $0.isNumber }
+    }
+    // if more than one ".", remove after second "."
+    let next = str[str.index(after: first)...].firstIndex(of: ".")
+    if let next = next {
+      let newStr = String.init(str.prefix(upTo: next))
+      // filter for numbers or "." and return
+      return newStr.filter { $0.isNumber || $0 == "." }
+    }
+    // if only one ".", filter for numbers or "." and return
+    return str.filter { $0.isNumber || $0 == "." }
+  }
+  
+  @Published var value = "" {
+    didSet {
+      value = trimStr(str: value)
+    }
+  }
+}
